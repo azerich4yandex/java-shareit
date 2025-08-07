62f253d0b52c64e1049bfca10e58c8b46601d34f
Rustam Aliev <azerich@yandex.ru>
2025-08-07 16:26:09 +0300

    modified schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS comments (
   text VARCHAR NOT NULL,
   item_id BIGINT NOT NULL,
   author_id BIGINT NOT NULL,
+  created TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   CONSTRAINT pk_comments PRIMARY KEY (id),
   CONSTRAINT fk_comments_to_items FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE,
   CONSTRAINT fk_comment_to_users FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
